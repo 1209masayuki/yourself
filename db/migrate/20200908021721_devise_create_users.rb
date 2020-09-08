e5b209c81247b2182a36e75dcf001c8beddf0cb7
1209masayuki <xkiddo22@yahoo.co.jp>
2020-09-08 11:29:58 +0900

    user_tableの設計

--- db/migrate/20200908021721_devise_create_users.rb
+++ db/migrate/20200908021721_devise_create_users.rb
@@ -3,6 +3,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :first_name,  null: false
+      t.string :family_name, null: false
+      t.date :birthday,  null: false
+      t.integer :age, null: false
+      t.string :phone_num, null: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
