e4c69a9bac4326d8317bd2dca13985a754fb10ef
1209masayuki <xkiddo22@yahoo.co.jp>
2020-09-09 10:07:04 +0900

    全体のコントローラーを管理しているapplicationコントローラーの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :family_name, :age, :birthday, :phone_num])
+  end
+  
 end
