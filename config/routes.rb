d9e37a3e8c28986b7c3e8a75ea4d3d0917625263
1209masayuki <xkiddo22@yahoo.co.jp>
2020-09-09 09:38:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "devise/registrations#new"
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+  resources :users, only: :index
+  root to: "users#index"
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
